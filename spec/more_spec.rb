2a464ba32bb8662911f3df26237457f61344bb8f
Bancobot <bancobot@bancodev.org>
2024-10-30 08:07:28 +0000

    We need more exclamations

---
+++ spec/more_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe Application do
+  include Rack::Test::Methods
+
+  subject(:app) { Rack::Builder.parse_file("config.ru") }
+
+  describe "GET /" do
+    it "says hello" do
+      get '/'
+
+      expect(last_response).to be_ok
+      expect(last_response.body).to match /Hello, World/
+    end
+  end
+
+  describe "GET /franky-says" do
+    it "quotes Franky" do
+      get '/franky-says'
+
+      expect(last_response).to be_ok
+      expect(last_response.body).to match /Take It Easy/
+    end
+  end
+end
